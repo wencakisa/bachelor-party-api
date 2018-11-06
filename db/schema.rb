1850771db5201d3df96601b13747a7cb4a17193c
wencakisa <wencakisa@gmail.com>
2018-11-06 22:44:59 +0200

    DB Schema with updated activity image_url field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_06_192056) do
+ActiveRecord::Schema.define(version: 2018_11_06_204336) do
 
   create_table "activities", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2018_11_06_192056) do
     t.integer "duration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
   create_table "users", force: :cascade do |t|
