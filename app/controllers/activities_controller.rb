43da7db09ccc06731975b3ba174cae66e19c3f7c
wencakisa <wencakisa@gmail.com>
2018-11-06 22:46:44 +0200

    Rendering JSON responses in activity controller

---
+++ app/controllers/activities_controller.rb
@@ -0,0 +1,47 @@
+class ActivitiesController < ApplicationController
+  before_action :set_activity, only: [:show, :update, :destroys]
+  load_and_authorize_resource
+
+  include Response
+
+  def index
+    @activites = Activity.all
+    json_response @activites
+  end
+
+  def show
+    json_response @activity
+  end
+
+  def create
+    @activity = Activity.create!(activity_params)
+    json_response @activity
+  end
+
+  def update
+    @activity.update(activity_params)
+    json_response @activity
+  end
+
+  def destroy
+    @activity.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_activity
+    @activity = Activity.find params[:id]
+  end
+
+  def activity_params
+    params.require(:activity).permit(
+      :title,
+      :subtitle,
+      :details,
+      :transfer_included,
+      :guide_included,
+      :duration
+    )
+  end
+end
