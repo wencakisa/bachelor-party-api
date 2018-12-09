b3603d321a0982b0714502a89880d9e9e32f3a2b
wencakisa <wencakisa@gmail.com>
2018-12-09 15:11:29 +0200

    Added indexes of activitties-quotations and activities-prices join tables

--- db/migrate/20181115202722_create_join_table_activities_quotations.rb
+++ db/migrate/20181115202722_create_join_table_activities_quotations.rb
@@ -1,7 +1,7 @@
 class CreateJoinTableActivitiesQuotations < ActiveRecord::Migration[5.2]
   def change
     create_join_table :activities, :quotations do |t|
-      # t.index [:activity_id, :quotation_id]
+      t.index [:activity_id, :quotation_id]
       # t.index [:quotation_id, :activity_id]
     end
   end
--- db/migrate/20181125100731_create_join_table_activities_prices.rb
+++ db/migrate/20181125100731_create_join_table_activities_prices.rb
@@ -1,7 +1,7 @@
 class CreateJoinTableActivitiesPrices < ActiveRecord::Migration[5.2]
   def change
     create_join_table :activities, :prices do |t|
-      # t.index [:activity_id, :price_id]
+      t.index [:activity_id, :price_id]
       # t.index [:price_id, :activity_id]
     end
   end
