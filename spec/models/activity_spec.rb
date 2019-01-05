697343a2edc9d45c2f33302ab74933c66d63d740
wencakisa <wencakisa@gmail.com>
2019-01-05 12:23:19 +0200

    Started testing the Activity model

---
+++ spec/models/activity_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+RSpec.describe Activity, type: :model do
+  subject(:activity) { create(:activity) }
+
+  describe 'associations' do
+    it 'should have and belong to many quotations' do
+      quotations = described_class.reflect_on_association(:quotations)
+      expect(quotations.macro).to eq(:has_and_belongs_to_many)
+    end
+
+    it 'should have many prices' do
+      prices = described_class.reflect_on_association(:prices)
+      expect(prices.macro).to eq(:has_many)
+    end
+  end
+
+  describe 'attributes' do
+    context 'when missing attributes' do
+      it 'is invalid without a title' do
+        activity.title = nil
+        expect(activity).to_not be_valid
+      end
+
+      it 'is invalid without a subtitle' do
+        activity.subtitle = nil
+        expect(activity).to_not be_valid
+      end
+
+      it 'is invalid without a duration' do
+        activity.duration = nil
+        expect(activity).to_not be_valid
+      end
+
+      it 'is invalid without a time type' do
+        activity.time_type = nil
+        expect(activity).to_not be_valid
+      end
+    end
+
+    context 'when attributes are invalid' do
+      it 'is invalid when title is too short' do
+        activity.title = ''
+        expect(activity).to_not be_valid
+      end
+
+      it 'is invalid when title is too long' do
+        activity.title = 'title' * 100
+        expect(activity).to_not be_valid
+      end
+
+      it 'is invalid when subtitle is too short' do
+        activity.subtitle = 'ok'
+        expect(activity).to_not be_valid
+      end
+
+      it 'is invalid when subtitle is too long' do
+        activity.subtitle = 'briefly' * 100
+        expect(activity).to_not be_valid
+      end
+
+      it 'is invalid when duration is a negative number' do
+        activity.duration = -1
+        expect(activity).to_not be_valid
+      end
+    end
+  end
+end
