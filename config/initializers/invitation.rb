a744a90df46241f261f6b30cb381ef8b3b0786b9
ventsislav_staykeepers <ventsislav@winwinnkeeper.net>
2019-02-16 14:06:02 +0200

    Added client registration url env variable

--- config/initializers/invitation.rb
+++ config/initializers/invitation.rb
@@ -1,4 +1,4 @@
 Invitation.configure do |config|
-  config.user_registration_url = ->(params) { Rails.application.credentials.client[:host] }
+  config.user_registration_url = ->(params) { Rails.application.credentials.client[:register_url] }
   config.mailer_sender = Rails.application.credentials.gmail[:username]
 end
