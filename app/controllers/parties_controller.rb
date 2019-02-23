9f6edeff40b595c1352c8e0b1d538d83c875b44d
wencakisa <wencakisa@gmail.com>
2019-02-23 20:48:27 +0200

    Party detail/update/destroy endpoints

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,6 +1,6 @@
 class PartiesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_activity, only: %i[show update destroy]
+  before_action :set_party, only: %i[show update destroy]
   load_and_authorize_resource
 
   def index
@@ -11,5 +11,29 @@ class PartiesController < ApplicationController
     end
 
     json_response @parties
+  end
+
+  def show
+    json_response @party
+  end
+
+  def update
+    @party.update(party_params)
+    json_response @party
+  end
+
+  def destroy
+    @party.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_party
+    @party = Party.find params[:id]
+  end
+
+  def party_params
+    params.require(:party).permit(:title, :datetime, :guide_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   resources :quotations, except: [:destroy]
 
-  resources :parties
+  resources :parties, except: [:create]
 
   resources :users, except: [:update]
 end
