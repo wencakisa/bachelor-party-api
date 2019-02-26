f2d9d1e3bd1b813b8d573cee308565dcfa525a97
wencakisa <wencakisa@gmail.com>
2019-02-26 19:08:53 +0200

    Added generic error json renderer

--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -2,4 +2,8 @@ module Response
   def json_response(object, status = :ok)
     render json: object, status: status
   end
+
+  def error_response(object, status = :unprocessable_entity)
+    render json: { errors: object.errors.full_messages }, status: status
+  end
 end
