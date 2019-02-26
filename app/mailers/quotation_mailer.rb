112abcc89cab5673427e63693f1859cc56b11f37
wencakisa <wencakisa@gmail.com>
2019-02-26 17:33:32 +0200

    Removed redundant quotation stuff

--- app/mailers/quotation_mailer.rb
+++
@@ -1,6 +0,0 @@
-class QuotationMailer < ApplicationMailer
-  def notify_customer_for_status_update(quotation)
-    @quotation = quotation
-    mail to: quotation.user_email, subject: 'Quotation ' + quotation.status
-  end
-end
--- db/migrate/20181218220540_add_custom_email_message_to_quotations.rb
+++
@@ -1,5 +0,0 @@
-class AddCustomEmailMessageToQuotations < ActiveRecord::Migration[5.2]
-  def change
-    add_column :quotations, :custom_email_message, :string
-  end
-end
