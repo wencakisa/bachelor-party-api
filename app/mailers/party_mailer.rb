bfb99bea06b48f78f992876977b5382d95a5a9e0
wencakisa <wencakisa@gmail.com>
2019-02-26 17:34:40 +0200

    Fixed identation in party mailer

--- app/mailers/party_mailer.rb
+++ app/mailers/party_mailer.rb
@@ -1,11 +1,11 @@
 class PartyMailer < ApplicationMailer
   def notify_guide_for_party_assignment(party)
-	@party = party
-	mail to: party.guide.email, subject: 'You have been assigned to ' + party.title.downcase
+  	@party = party
+  	mail to: party.guide.email, subject: 'You have been assigned to ' + party.title.downcase
   end
 
   def notify_guide_for_party_withdrawal(party, guide_email)
-	@party = party
-	mail to: guide_email, subject: 'You have been withdrawn from ' + party.title.downcase
+  	@party = party
+  	mail to: guide_email, subject: 'You have been withdrawn from ' + party.title.downcase
   end
 end
