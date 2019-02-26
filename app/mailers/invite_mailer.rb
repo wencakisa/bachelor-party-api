c506ec722705c5b9d5ad98b8a8c3d9de041b0df3
wencakisa <wencakisa@gmail.com>
2019-02-26 18:01:56 +0200

    More detailed information in invite email templates

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -5,6 +5,12 @@ class InviteMailer < ApplicationMailer
     client_api_url = Rails.application.credentials.client[:register_url]
     @url = "#{client_api_url}?invitation_token=#{@invite.token}"
 
-    mail to: @invite.email, subject: 'Invite for ' + @invite.invitable_type
+    if @invite.invitable_type == 'Party'
+      @party = @invite.invitable
+      mail to: @invite.email, subject: "Invite for #{@party.title}"
+    elsif @invite.invitable_type == 'Quotation'
+      @quotation = @invite.invitable
+      mail to: @invite.email, subject: "Your quotation has been #{@quotation.status}"
+    end
   end
 end
