caee3b6cc28b8d168b8bb23e652434dad8f22d8d
wencakisa <wencakisa@gmail.com>
2019-05-05 14:24:34 +0300

    User factory with nested factory for admins

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    email    { Faker::Internet.email }
+    password { Faker::Internet.password }
+
+    factory :admin do
+      role { :admin }
+    end
+  end
+end
