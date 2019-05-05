4514d9c06d3d1d1ab8a7d19c37135021b59f1372
wencakisa <wencakisa@gmail.com>
2019-05-06 00:06:20 +0300

    Additional RSpec helpers inclusion

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,3 +1,8 @@
+require 'helpers/api_helper'
+require 'helpers/auth_helper'
+
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+
+  config.include ApiHelper, AuthHelper, type: :controller
 end
