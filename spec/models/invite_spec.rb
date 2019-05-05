d830bca69a475d638bf3615b302577b1f122107f
wencakisa <wencakisa@gmail.com>
2019-05-05 14:26:12 +0300

    Invites specs

--- spec/models/invite_spec.rb
+++ spec/models/invite_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe Invite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Invite, type: :model do
+  describe '#generate_token' do
+    it 'generates secure token after created' do
+      invite = create(:invite)
+
+      expect(invite.token).to be_truthy
+    end
+  end
+
+  describe '#set_recipient_if_exists' do
+    it 'sets recipient if user with the same email already exists' do
+      email = 'user@email.com'
+      create(:user, email: email)
+      invite = create(:invite, email: email)
+
+      expect(invite.recipient).to be_truthy
+      expect(invite.recipient.email).to eq email
+    end
+  end
+
+  # describe '#notify_recipient' do
+  #   it 'notifies recipient when invite is generated' do
+  #     invite = create(:invite)
+
+  #     message_delivery = instance_double(ActionMailer::MessageDelivery)
+  #     expect(InviteMailer).to receive(:new_user_invite).with(invite).and_return(message_delivery)
+  #     allow(message_delivery).to receive(:deliver_later)
+  #   end
+  # end
 end
