34a9223752af3bca4e7555e37167fc690d195b22
wencakisa <wencakisa@gmail.com>
2019-05-05 12:22:07 +0300

    Added date attribute to Quotations factory

--- spec/factories/quotations.rb
+++ spec/factories/quotations.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :quotation do
     group_size { Faker::Number.between(1, 50) }
     user_email { Faker::Internet.email }
+    date       { Faker::Date.forward }
     activities { create_list(:activity, 5) }
     prices     { activities.map(&:prices).map(&:first) }
   end
