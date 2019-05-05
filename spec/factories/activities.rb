5b35f0f0f9fef40d595976684929323474a5dabf
wencakisa <wencakisa@gmail.com>
2019-05-05 14:23:45 +0300

    Using Faker::Name for mocking activity titles

--- spec/factories/activities.rb
+++ spec/factories/activities.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :activity do
-    title     { Faker::App.unique.name }
+    title     { Faker::Name.unique.name }
     subtitle  { Faker::Company.catch_phrase }
     duration  { Faker::Number.between(1, 50) }
     time_type { :day }
