183602bcda3eaa90f4c001e94e19a4b8d06f9509
wencakisa <wencakisa@gmail.com>
2019-05-05 18:22:44 +0300

    Removed UserParty specs & factories as it is join model

--- spec/factories/user_parties.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :user_party do
-    user { nil }
-    party { nil }
-  end
-end
--- spec/models/user_party_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-describe UserParty, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
