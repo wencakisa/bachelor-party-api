4dd7142cdfe1d4fdf829243b92ffa5ab47981f31
wencakisa <wencakisa@gmail.com>
2019-05-05 18:59:14 +0300

    Simplified party mailer and reduced it to one generic method

--- app/mailers/party_mailer.rb
+++ app/mailers/party_mailer.rb
@@ -1,11 +1,19 @@
 class PartyMailer < ApplicationMailer
   def notify_guide_for_party_assignment(party)
-  	@party = party
-  	mail to: party.guide.email, subject: 'You have been assigned to ' + party.title.downcase
+    send_mail(party, party.guide.email, 'assigned to')
   end
 
   def notify_guide_for_party_withdrawal(party, guide_email)
-  	@party = party
-  	mail to: guide_email, subject: 'You have been withdrawn from ' + party.title.downcase
+    send_mail(party, guide_email, 'withdrawn from')
+  end
+
+  private
+
+  def send_mail(party, guide_email, action)
+    @party = party
+    mail(
+      to: guide_email,
+      subject: "You have been #{action} the #{party.title.humanize}"
+    )
   end
 end
