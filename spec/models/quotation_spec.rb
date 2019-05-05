f4f69fb7ded2c14d1cab3b1d4cde3bd181ef4c4e
wencakisa <wencakisa@gmail.com>
2019-05-05 14:25:26 +0300

    Refactored quotations specs

--- spec/models/quotation_spec.rb
+++ spec/models/quotation_spec.rb
@@ -1,58 +1,41 @@
 require 'rails_helper'
 
-RSpec.describe Quotation, type: :model do
+describe Quotation, 'associations', type: :model do
   subject(:quotation) { create(:quotation) }
 
-  describe 'associations' do
-    it 'has and belongs to many activities' do
-      activities = described_class.reflect_on_association(:activities)
-      expect(activities.macro).to eq(:has_and_belongs_to_many)
-    end
-
-    it 'has and belongs to many prices' do
-      prices = described_class.reflect_on_association(:prices)
-      expect(prices.macro).to eq(:has_and_belongs_to_many)
-    end
+  it 'has and belongs to many activities' do
+    activities = described_class.reflect_on_association(:activities)
+    expect(activities.macro).to eq(:has_and_belongs_to_many)
   end
 
-  describe 'attributes' do
-    context 'when missing' do
-      it 'is invalid without a group size' do
-        quotation.group_size = nil
-        expect(quotation).to_not be_valid
-      end
+  it 'has and belongs to many prices' do
+    prices = described_class.reflect_on_association(:prices)
+    expect(prices.macro).to eq(:has_and_belongs_to_many)
+  end
+end
 
-      it 'is invalid without a user email' do
-        quotation.user_email = nil
-        expect(quotation).to_not be_valid
-      end
+describe Quotation, 'attributes', type: :model do
+  subject(:quotation) { create(:quotation) }
 
-      it 'is invalid without activities' do
-        quotation.activities = []
-        expect(quotation).to_not be_valid
-      end
-
-      it 'is invalid without prices' do
-        quotation.prices = []
-        expect(quotation).to_not be_valid
-      end
+  context 'when missing' do
+    it 'is invalid without a group size' do
+      quotation.group_size = nil
+      expect(quotation).to_not be_valid
     end
 
-    context 'when invalid' do
-      it 'is invalid when group size is not an integer' do
-        quotation.group_size = 1.23
-        expect(quotation).to_not be_valid
-      end
+    it 'is invalid without a user email' do
+      quotation.user_email = nil
+      expect(quotation).to_not be_valid
+    end
 
-      it 'is invalid when group size is a negative number' do
-        quotation.group_size = -1
-        expect(quotation).to_not be_valid
-      end
+    it 'is invalid without activities' do
+      quotation.activities = []
+      expect(quotation).to_not be_valid
+    end
 
-      it 'is invalid when group size is too big' do
-        quotation.group_size = 100
-        expect(quotation).to_not be_valid
-      end
+    it 'is invalid without prices' do
+      quotation.prices = []
+      expect(quotation).to_not be_valid
     end
   end
 
@@ -62,16 +45,39 @@ RSpec.describe Quotation, type: :model do
     it 'is invalid when any activity does not have prices' do
       quotation.activities.first.prices = []
       expect(quotation).to_not be_valid
+      expect(quotation.errors).to include 'activities'
     end
 
     it 'is invalid when activities do not have single price chosen' do
       quotation.prices << price
       expect(quotation).to_not be_valid
+      expect(quotation.errors).to include 'activities'
     end
 
     it 'is invalid when prices are not valid for each activity' do
       quotation.prices.last.delete
       quotation.prices << price
+      expect(quotation).to_not be_valid
+    end
+  end
+end
+
+describe Quotation, 'group size validations', type: :model do
+  subject(:quotation) { create(:quotation) }
+
+  context 'is invalid when group size' do
+    it 'is not an integer' do
+      quotation.group_size = 1.23
+      expect(quotation).to_not be_valid
+    end
+
+    it 'is a negative number' do
+      quotation.group_size = -1
+      expect(quotation).to_not be_valid
+    end
+
+    it 'is too big' do
+      quotation.group_size = 100
       expect(quotation).to_not be_valid
     end
   end
