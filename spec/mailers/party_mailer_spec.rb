718514a1b9de426114664c6ca89cb75a1ea38009
wencakisa <wencakisa@gmail.com>
2019-05-05 19:00:02 +0300

    Party mailer specs (wip)

--- spec/mailers/party_mailer_spec.rb
+++ spec/mailers/party_mailer_spec.rb
@@ -1,5 +1,33 @@
-require "rails_helper"
+require 'rails_helper'
 
-describe PartyMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe PartyMailer, '#notify_guide_for_party_assignment', type: :mailer do
+  let(:guide) { create(:guide) }
+  let(:host)  { create(:host) }
+  let(:party) { create(:party, guide: guide, host: host) }
+  let(:mail)  { PartyMailer.notify_guide_for_party_assignment(party) }
+
+  it 'sets proper action in subject' do
+    expect(mail.subject).to include('assigned')
+  end
+
+  it 'adds party title to subject' do
+    expect(mail.subject).to include('Party of')
+    # expect(mail.subject).to include(party.host.email)
+  end
+end
+
+describe PartyMailer, '#notify_guide_for_party_withdrawal', type: :mailer do
+  let(:guide) { create(:guide) }
+  let(:host)  { create(:host) }
+  let(:party) { create(:party, host: host) }
+  let(:mail)  { PartyMailer.notify_guide_for_party_withdrawal(party, guide.email) }
+
+  it 'sets proper action in subject' do
+    expect(mail.subject).to include('withdrawn')
+  end
+
+  it 'adds party title to subject' do
+    expect(mail.subject).to include('Party of')
+    # expect(mail.subject).to include(party.host.email)
+  end
 end
