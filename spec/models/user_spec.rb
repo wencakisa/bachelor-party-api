0611d41462f0c15a0543025fa54356529f665566
wencakisa <wencakisa@gmail.com>
2019-05-06 15:43:14 +0300

    Refactored User model spec and implemented User#parties cases

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
-describe User, '.claim_invite', type: :model do
-  let(:invite) { create(:invite) }
-  let(:user) { create(:user) }
+describe User, type: :model do
+  subject(:user) { create(:user) }
+  let(:invite)   { create(:invite) }
   let(:claim_params) do
     {
       invitation_token: invite.token,
@@ -9,30 +9,36 @@ describe User, '.claim_invite', type: :model do
     }
   end
 
-  it 'claims invitation when invite exists' do
-    expect { User.claim_invitation(claim_params) }.to change { User.count }.by(3)
-    expect(Invite.last.status).to eq 'accepted'
+  describe '.claim_invite' do
+    it 'claims invitation when invite exists' do
+      expect do
+        User.claim_invitation(claim_params)
+      end.to change { User.count }.by(3)
+      expect(Invite.last.status).to eq 'accepted'
+    end
+
+    it 'sets invite recipient to invited user' do
+      user = User.claim_invitation(claim_params)
+      expect(user).to eq Invite.last.recipient
+    end
+
+    it 'adds invite to user invitations' do
+      user = User.claim_invitation(claim_params)
+      expect(user.invitations.size).to eq 1
+      expect(user.invitations.last.recipient).to eq user
+    end
   end
 
-  it 'sets invite recipient to invited user' do
-    user = User.claim_invitation(claim_params)
-    expect(user).to eq Invite.last.recipient
-  end
+  describe '#parties' do
+    context 'when user is guide' do
+      subject(:guide) { create(:guide) }
+      let(:parties)   { create_list(:party, 3, guide: guide) }
 
-  it 'adds invite to user invitations' do
-    user = User.claim_invitation(claim_params)
-    expect(user.invitations.size).to eq 1
-    expect(user.invitations.last.recipient).to eq user
+      before { create_list(:party, 2) }
+
+      it 'returns only guide parties' do
+        expect(guide.parties).to eq parties
+      end
+    end
   end
 end
-
-# describe User, '#parties', type: :model do
-#   subject(:customer) { create(:customer) }
-#   subject(:guide) { create(:guide) }
-
-#   context 'when user is guide' do
-#     it 'returns only guide parties' do
-#       expect(guide.parties).to eq
-#     end
-#   end
-# end
