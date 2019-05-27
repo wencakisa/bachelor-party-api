530dbc3bb7e7e1f210a40228bd47ca89dc682adb
kall0m <k.madjunov@gmail.com>
2019-05-27 17:39:18 +0300

    Add missing properties of activity in serializer

--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -1,5 +1,5 @@
 class ActivitySerializer < ActiveModel::Serializer
-  attributes :id, :title, :subtitle, :duration, :time_type, :image_url
+  attributes :id, :title, :subtitle, :details, :duration, :transfer_included, :guide_included, :image_url, :time_type
 
   has_many :prices
 end
